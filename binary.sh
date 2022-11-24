e7388f61bd3252297be447139e6a3d199a8c33fd
lxthyme <lx314333@gmail.com>
2022-11-24 18:59:15 +0800

    17

--- binary.sh
+++ binary.sh
@@ -38,7 +38,7 @@ esac
 cd ~/.cocoapods/repos/LXRepo
 git pull origin $branch
 cd -
-pod repo push LXRepo $PROJECT_NAME.podspec --verbose --allow-warnings --use-libraries --sources='git@github.com:lxthyme/LXRepo.git'
+pod repo push LXRepo $PROJECT_NAME.podspec --verbose --allow-warnings --use-libraries # --sources='git@github.com:lxthyme/LXRepo.git'
 #,https://cdn.cocoapods.org/'
 
 # pod bin auto --configuration=Release --env=debug_iphoneos
